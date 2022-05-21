489a965de4338ddd1995205dbbadca3f5477ee6e
subramaniyant7 <72293520+subramaniyant7@users.noreply.github.com>
2022-05-21 11:09:29 +0530

    .

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -92,7 +92,7 @@
                         <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Hospital</p>
+                                <p>Hospitality</p>
                             </a>
                         </li>
                     </ul>
