16f99f343581a8f349512af3ff83fbbd57509cfb
subramaniyant7 <tsubramaniyan2@gmail.com>
2022-07-30 16:46:28 +0530

    Clients

--- resources/views/frontend/ourclients.blade.php
+++ resources/views/frontend/ourclients.blade.php
@@ -3,8 +3,28 @@
 
 @section('content')
 
-@include('frontend.clients')
-
+<!-- Start Partner Area -->
+<div class="partner-area ptb-80 bg">
+    <div class="container">
+        <div class="row">
+            <div class="section-title">
+                <h3>Our Clients</h3>
+            </div>
+            <div class="partner-slider">
+                @foreach (getActiveRecord('client_details') as $client)
+                <div class="col-lg-12 col-md-12">
+                    <div class="item">
+                        <a href="#">
+                            <img src="{{ URL::asset('uploads/clients/'.$client->client_logo) }}" alt="Our Clients">
+                        </a>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Partner Area -->
 
 
 @stop
